52ff77aaeb81cd071d1434dfe5e419ba707af47a
Dan <dvwhite1981@gmail.com>
2023-08-22 14:15:42 -0400

    Bonus - Complete

--- sql-practice/aggregates.sql
+++ sql-practice/aggregates.sql
@@ -1,8 +1,8 @@
--- Step 1: Count
+-- Step 1: COUNT
 SELECT COUNT(*) FROM cats;
 
 
--- Step 2: Min/Max
+-- Step 2: MIN/MAX
 SELECT name, MIN(birth_year) AS oldest
     FROM cats;
 
@@ -17,3 +17,28 @@ SELECT name, birth_year
         (SELECT MAX(birth_year) FROM cats)
     )
     ORDER BY birth_year;
+
+
+-- BONUS
+
+-- Step 1: GROUP BY
+SELECT cats.name, COUNT(*)
+    FROM cats
+    JOIN toys ON (cats.id = cat_id)
+    GROUP BY cats.name;
+
+
+-- Step 2: HAVING
+-- Just names
+SELECT cats.name
+    FROM cats
+    JOIN toys ON (cats.id = cat_id)
+    GROUP BY cats.name
+    HAVING COUNT(*) > 1;
+
+-- With counts
+SELECT cats.name, COUNT(*)
+    FROM cats
+    JOIN toys ON (cats.id = cat_id)
+    GROUP BY cats.name
+    HAVING COUNT(*) > 1;
